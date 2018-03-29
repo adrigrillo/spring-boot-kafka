cb49072837668245322c5c15369f3f27503fe1f7
adrigrillo <adrirgrillo@gmail.com>
2018-03-29 20:05:05 +0200

    First try

---
+++ producer/src/main/java/com/example/producer/MessageProducer.java
@@ -0,0 +1,34 @@
+package com.example.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Component
+@RestController
+@EnableBinding(Source.class)
+public class MessageProducer {
+
+    private Source source;
+
+    @Autowired
+    public MessageProducer(Source source) {
+        this.source = source;
+    }
+
+    @RequestMapping("/send")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    private void createMessage(@RequestParam(value = "msg") String message) {
+        log.info("Received petition, sending message. Message to send {0}", message);
+        source.output().send(MessageBuilder.withPayload(message).build());
+    }
+}
---
+++ receiver/src/main/java/com/example/receiver/MessageReceiver.java
@@ -0,0 +1,18 @@
+package com.example.receiver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@EnableBinding(Sink.class)
+public class MessageReceiver {
+
+    @StreamListener(Sink.INPUT)
+    private void receiveMessage(String message) {
+        log.info("Message received from Kafka: {0}", message);
+    }
+}
